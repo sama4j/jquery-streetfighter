52101e861f02c50c01a868b949626732067ab09f
sama4j <sama4j@users.noreply.github.com>
2014-07-14 20:15:11 -0700

    street fighter final
    final completed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 	$('.ryu').mouseenter(function() {
+		$('.ryu-cool').hide();
+		$('.ryu-still').hide();
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
+		$('.ryu-cool').hide();
+		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
 		//play hadouken sound
 		playHadouken();
+		$('.ryu-cool').hide();
+		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
@@ -22,8 +28,26 @@ $(document).ready(function(){
 			});
 	})
 	.mouseup(function() {
+		$('.ryu-cool').hide();
+		$('.ryu-still').hide();
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+	});
+	
+	$("body").keydown(function(event) {
+		if (event.which == 88) {
+			event.preventDefault();
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		};
+		$(this).keyup(function(event) {
+			if (event.which == 88) {
+				event.preventDefault();
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			};
+		});
 	});
 });
 
